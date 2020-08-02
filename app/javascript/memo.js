fdf464c30474d123d87f424b0c6218b3e2427435
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-02 23:15:41 +0900

    リクエストの処理

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,10 @@
 function memo() {
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {})
+  submit.addEventListener("click", (e) => {
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  })
 }
 window.addEventListener("load", memo);
